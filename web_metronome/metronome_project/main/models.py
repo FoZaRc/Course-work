cfe78c7a686f61b6f4eeece2ad58dd27fe324886
glensaren <imaf7024@gmail.com>
2025-01-16 09:35:02 +0300

    вроде как рабочий

---
+++ web_metronome/metronome_project/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+class feedback(models.Model):
+    name = models.CharField('Имя пользователя', max_length=30)
+    email = models.EmailField('E-mail пользователя', max_length=50)
+    feedback_text = models.TextField('Текст отзыва')
+    created_at = models.DateTimeField('Время написания отзыва', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+    
