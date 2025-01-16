8b10f66dd4087b5e1bd3be2ae98d6244a122b817
glensaren <imaf7024@gmail.com>
2025-01-16 14:13:55 +0300

    Update settings.py

--- web_metronome/metronome_project/metronome_project/settings.py
+++ web_metronome/metronome_project/metronome_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mbwb27foamkvujbn+&eyqd&()prt8h-i&m(1_fnx=sr7k7y8v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
